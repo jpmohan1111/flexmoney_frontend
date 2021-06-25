68e915f40e5606a609ea846f268ffff82bbe917e
vijay_togglehead <webdevelopment@togglehead.in>
2021-06-25 12:39:49 +0530

    lenders sec 2 change

--- src/container/Lenders/Lenders.js
+++ src/container/Lenders/Lenders.js
@@ -119,7 +119,7 @@ const sectionTwoDataArr = [
   },
   {
     desc: "Offer Purchase financing to all existing customer segments through Mobile number based Cardless experience",
-    rightImg: flipkart,
+    rightImg: rightimg,
     mbImg: mbrightimg,
   },
   {
